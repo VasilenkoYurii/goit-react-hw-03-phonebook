27cdc994d70a071bbc5c35b9d701c2d88c574424
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-04-03 13:21:02 +0300

    add localStorage in contacts

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,24 @@ import ContactForm from 'components/ContactForm/ContactForm';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = values => {
     for (const contact of this.state.contacts) {
